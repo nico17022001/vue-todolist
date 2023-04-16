d11edd070b5f48cf2cceaa5290acc9944c32946f
nico17022001 <nico17022001boolean@gmail.com>
2023-04-16 18:01:26 +0200

    fine-esercizio

--- script.js
+++ script.js
@@ -2,17 +2,48 @@ const {createApp} = Vue
 
 createApp({
 
-
   data(){
     return {
+      daFare:"",
 
+      lista:[
+        {
+          testo:"giallo",
+          fatto: false,
+        },
+
+        {
+          testo:"pino",
+          fatto: false,
+        },
+
+        {
+          testo:"franco",
+          fatto: false,
+        },
+      ],
     }
   },
 
   methods: {
-    
+    elimina(i,elemento){
+      if (elemento.fatto == true) {
+        this.lista.splice(i,1)
+      }
+    },
+
+
+
+    aggiungiTask(){
+      const elemento = {
+        testo: this.daFare,
+        fatto: false,
+      }
+      this.lista.unshift(elemento)
+      this.daFare = ""
+    }
   }
-}).mmount("app")
+}).mount("#app")
 
 
 
